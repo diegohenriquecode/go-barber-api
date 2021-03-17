8bf4018264fa88f2f2ba088da002c8b616c1f22f
diegoholiveirav <diegoholiveirav@gmail.com>
2021-03-16 22:04:20 -0300

    configuracao completa v01

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const CreateAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '1234567890987654321',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234567890987654321');
+  });
 });
+
+// describe('CreateAppointment', () => {
+//   it('should not be able to create two appointments on the same Date', () => {
+//     expect(1 + 2).toBe(3);
+//   });
+// });
